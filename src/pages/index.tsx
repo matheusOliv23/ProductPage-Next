e81d4777a81a77166e5505f2c9667e9afe07292f
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-11 21:15:11 -0300

    feat: add home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "src/styles/pages/home";
+
+import shirt01 from "src/assets/01.png";
+import shirt02 from "src/assets/02.png";
+import shirt03 from "src/assets/03.png";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image alt="Camisetas" width={520} height={480} src={shirt01} />
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 49,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
